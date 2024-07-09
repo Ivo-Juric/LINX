4903a836a058ccf84b3c3e1fccc021beea4b0a15
NicoEtcheverry <nicolasetcheverry2000@gmail.com>
2024-07-09 19:56:49 +0000

    ivo down

--- Athlete.java
+++ Athlete.java
@@ -19,6 +19,8 @@ public class Athlete {
 		this.gender = gender;
 		this.category = category;
 		physicalCondition = new PhysicalCondition(physicalCondition.getSpeed(), physicalCondition.getEndurance());	
+		physicalCondition = new PhysicalCondition(physicalCondition.getSpeed(), physicalCondition.getEndurance());	
+		physicalCondition = new PhysicalCondition(physicalCondition.getSpeed(), physicalCondition.getEndurance());	
 	}
 }
 
